e14dd56dc7d49a20da7e7f19200cce265f479ae7
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-23 16:35:06 +0700

    feat: Revisi server

---
+++ server/models/post.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Post extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Post.init(
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+			description: DataTypes.STRING,
+			imageUrl: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Post",
+		}
+	);
+	return Post;
+};
